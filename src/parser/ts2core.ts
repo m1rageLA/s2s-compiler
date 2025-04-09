dbcc8aec7221ce7f858dc148f0ff6d9576fe6148
m1rage <miragela.code@gmail.com>
2025-04-09 15:21:16 +0200

    chore: remove typescript-transpiller folder

--- src/parser/ts2core.ts
+++ src/parser/ts2core.ts
@@ -104,3 +104,31 @@ export function transpileFileToIR(filePath: string): IRNode {
     }
     return { kind: "Program", body };
 }
+
+//Statement Converters (ts.Node -> IRNode)
+function convertStatement(node: ts.Node): IRNode {
+    switch (node.kind) {
+        case ts.SyntaxKind.FunctionDeclaration:
+            return convertFunctionDeclaration(node as ts.FunctionDeclaration);
+        case ts.SyntaxKind.VariableDeclaration:
+            return convertVariableDeclaration(node as ts.VariableDeclaration);
+        case ts.SyntaxKind.ReturnStatement:
+            return convertReturnStatement(node as ts.ReturnStatement);
+        case ts.SyntaxKind.IfStatement:
+            return convertIfstatement(node as ts.IfStatement);
+        case ts.SyntaxKind.WhileKeyword:
+            return convertWhileKeyword(node as ts.WhileStatement);
+        case ts.SyntaxKind.ForStatement:
+            return convertForStatement(node as ts.ForStatement);
+        case ts.SyntaxKind.Block:
+            return convertBlock(node as ts.Block);
+        case ts.SyntaxKind.ExpressionStatement:
+            return {
+                kind: "ExpressionStatement",
+                expression: convertExpression((node as ts.ExpressionStatement).expression),
+            };
+
+        default:
+            throw new Error("Unsupported node kind: " + ts.SyntaxKind[node.kind]);
+    }
+}
