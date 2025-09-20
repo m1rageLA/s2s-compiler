27270ccce1580fb2288502dfe2732f1cd6440b28
m1rage <miragela.code@gmail.com>
2025-09-20 11:49:55 +0200

    [feat](ir): add initial function processing to the IR
    params transformation

--- src/lowering/src/lib.rs
+++ src/lowering/src/lib.rs
@@ -1,5 +1,7 @@
+use std::array;
+
 use ir::*;
-use swc_ecma_ast as ast;
+use swc_ecma_ast::{self as ast, Number, Str};
 
 pub fn ast_to_ir(module: &ast::Module) -> IrModule {
     let mut items: Vec<IrItem> = Vec::new();
@@ -12,6 +14,10 @@ pub fn ast_to_ir(module: &ast::Module) -> IrModule {
                         items.push(IrItem::Variable(ir_var));
                     }
                 }
+            }
+            ast::ModuleItem::Stmt(ast::Stmt::Decl(ast::Decl::Fn(fn_decl))) => {
+                for param in &fn_decl.function.params {}
+                if let Some(ir_fn) = fn_decl_to_ir(fn_decl) {}
             }
             _ => (),
         }
@@ -46,7 +52,9 @@ fn var_decl_to_ir(decl: &ast::VarDeclarator) -> Option<IrVariable> {
 
     let init = decl.init.as_ref().map(|expr| match &**expr {
         ast::Expr::Lit(ast::Lit::Num(n)) => IrExpression::Literal(IrLiteral::Int(n.value as i32)),
-        ast::Expr::Lit(ast::Lit::Str(s)) => IrExpression::Literal(IrLiteral::Str(s.value.to_string())),
+        ast::Expr::Lit(ast::Lit::Str(s)) => {
+            IrExpression::Literal(IrLiteral::Str(s.value.to_string()))
+        }
         _ => IrExpression::Identifier("unsupported".to_string()),
     });
 
@@ -56,3 +64,37 @@ fn var_decl_to_ir(decl: &ast::VarDeclarator) -> Option<IrVariable> {
         value: init,
     })
 }
+
+fn fn_decl_to_ir(fn_decl: &ast::FnDecl) -> Option<IrFunction> {
+    /*
+        pub name: String,
+        pub params: Vec<IrParam>,
+        pub body: Vec<IrStmt>,
+    */
+    let name: String = fn_decl.ident.sym.to_string();
+
+    let mut params: Vec<IrParam> = Vec::new();
+    for p in &fn_decl.function.params {
+        match &p.pat {
+            ast::Pat::Ident(ast::BindingIdent { id, type_ann }) => {
+                let pname = id.sym.to_string();
+                let pty = type_ann
+                    .as_ref()
+                    .map(|ann| ts_type_ann_to_ir(ann))
+                    .unwrap_or(IrType::Any);
+                params.push(IrParam {
+                    name: pname,
+                    ty: pty,
+                });
+            }
+            _ => return None,
+        }
+    }
+    Some(IrFunction {
+        name,
+        params,
+        // ret: ret_ty,          // TODO: раскомментируйте, если в вашей IR есть поле возвращаемого типа
+        body: Vec::new(), // TODO: замените на тип вашей IR (например, None)
+    })
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-
-
 fn main() {
-    lowering::ast_to_ir(&parser::ast("let x: number = 10"));
+    // lowering::ast_to_ir(&parser::ast("let x: number = 10"));
+    let ast = parser::ast("function lib(x: number, y: number) {return x+ y}");
+    println!("{:#?}", ast);
 }
+
+//TODO имплементировать лог ошибок компилятора ts
