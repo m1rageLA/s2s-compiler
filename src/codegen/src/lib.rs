88b185ea1cf261857d3982c493fc955b9a9ac47d
m1rage <miragela.code@gmail.com>
2025-09-27 14:44:03 +0200

    [feat](codegen): initial codegen with proc_macro2 + quote

--- src/codegen/src/lib.rs
+++ src/codegen/src/lib.rs
@@ -1,3 +1,35 @@
-pub fn gen_rust() {
-    println!("Hello, world!");
+use ir::*;
+use proc_macro2::{Ident, Span, TokenStream};
+use quote::quote;
+
+pub fn gen_module(m: &IrModule) -> TokenStream {
+    // только переменные -> в main
+    let decls = m.items.iter().filter_map(|item| match item {
+        IrItem::Variable(v) => Some(gen_var_decl(v)),
+        _ => None,
+    });
+
+    quote! {
+        fn main() {
+            #(#decls)*
+        }
+    }
+}
+
+fn gen_var_decl(var: &IrVariable) -> TokenStream {
+    let name = Ident::new(&var.name, Span::call_site());
+    let ty = gen_type(&var.ty);
+    quote! {
+        let #name: #ty;
+    }
+}
+
+pub fn gen_type(ty: &IrType) -> TokenStream {
+    match ty {
+        IrType::Int => quote! { i32 },
+        IrType::Str => quote! { ::std::string::String },
+        IrType::Bool => quote! { bool },
+        IrType::Unit => quote! { () },
+        IrType::Any => quote! { /* any */ () },
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,15 @@
 fn main() {
     let x1 = "function lib(x: number, y: number): number {return x+ y}";
 
-    let x2 = "
-    let x: number = 5;
-    let y: string = \"hello\";
-
-    function add(a: number, b: number): number {
-        let c: number = a + b;
-        return c;
-    }
-
-    {
-        let z: boolean = true;
-        x = x + 1;
-    }
-";
+    let x2 = " let x: number; ";
 
     let code = x2;
 
     let ir = lowering::ast_to_ir(&parser::ast(code));
     let ast: swc_ecma_ast::Module = parser::ast(code);
-    let rust = codegen::gen_rust();
+    let rust = codegen::gen_module(&ir);
 
-    println!("{:#?}", rust);
+    println!("{}", rust);
 }
 
 //TODO имплементировать лог ошибок компилятора ts
