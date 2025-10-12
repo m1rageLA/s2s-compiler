21e381662b2d89c6d925b2600890f282f5b6fac6
m1rage <miragela.code@gmail.com>
2025-10-12 13:18:41 +0200

    [feat](cli): add new argument for displaying the AST tree

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,22 @@ use std::time::Instant;
 const DEFAULT_TS_PATH: &str = "ts/program.ts";
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() > 1 {
+        match args[1].as_str() {
+            "ast" => {
+                run_ast();
+                return;
+            }
+            _ => 
+            { 
+                run_pipeline();
+                return;
+            }
+        }
+    }
+
     if cfg!(debug_assertions) {
         println!("[debug] Detected debug build; launching release run for final output...");
         match launch_release_run() {
@@ -29,6 +45,17 @@ fn main() {
     run_pipeline();
 }
 
+fn run_ast() {
+    let (ts_path, ts_source) = match load_typescript_source() {
+        Ok(result) => result,
+        Err(err) => {
+            eprintln!("    [error] {err}");
+            std::process::exit(1);
+        }
+    };
+    let ast = parser::ast(&ts_source);
+    println!("{:#?}", ast);
+}
 fn run_pipeline() {
     let total_stages = 5;
     let pipeline_start = Instant::now();
@@ -57,7 +84,6 @@ fn run_pipeline() {
     // log_stage(2, total_stages, "Parsing source into AST");
     let ast_timer = Instant::now();
     let ast = parser::ast(&ts_source);
-    // println!("    [ok] AST parsed in {:.2?}", ast_timer.elapsed());
 
     // log_stage(3, total_stages, "Lowering AST to intermediate representation");
     let lowering_timer = Instant::now();
@@ -111,7 +137,10 @@ fn run_pipeline() {
             // );
         }
         Err(err) => {
-            eprintln!("    [error] Generated program failed:\n{}", indent_multiline(&err));
+            eprintln!(
+                "    [error] Generated program failed:\n{}",
+                indent_multiline(&err)
+            );
             std::process::exit(1);
         }
     }
@@ -123,7 +152,7 @@ fn run_pipeline() {
 }
 
 fn load_typescript_source() -> Result<(PathBuf, String), String> {
-    let path_arg = env::args().nth(1);
+    let path_arg = env::args().nth(2);
     let path = path_arg
         .map(PathBuf::from)
         .unwrap_or_else(|| PathBuf::from(DEFAULT_TS_PATH));
@@ -135,8 +164,7 @@ fn load_typescript_source() -> Result<(PathBuf, String), String> {
 fn run_generated(rust: &str) -> Result<String, String> {
     // println!("    -> Writing generated Rust to out/src/main.rs");
     fs::create_dir_all("out/src").map_err(|e| format!("create out dir failed: {e}"))?;
-    fs::write("out/src/main.rs", rust)
-        .map_err(|e| format!("write out/src/main.rs failed: {e}"))?;
+    fs::write("out/src/main.rs", rust).map_err(|e| format!("write out/src/main.rs failed: {e}"))?;
 
     // println!("    -> Running cargo run --release in ./out");
     let run = Command::new("cargo")
