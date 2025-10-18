3a51544c22986f9bfd7fadc4e7b1d8f59f593a21
m1rage <miragela.code@gmail.com>
2025-10-18 19:32:54 +0200

    [test](codegen): add array unit test

--- crates/codegen/src/expr/array.rs
+++ crates/codegen/src/expr/array.rs
@@ -7,3 +7,15 @@ pub(crate) fn array_tokens(items: &[IrExpression]) -> TokenStream {
     let item_tokens: Vec<TokenStream> = items.iter().map(|item| item.codegen()).collect();
     quote! { [ #( #item_tokens ),* ] }
 }
+
+#[test]
+fn test_array_tokens() {
+    let items = vec![
+        IrExpression::Literal(ir::IrLiteral::Number(1.0)),
+        IrExpression::Literal(ir::IrLiteral::Number(2.0)),
+        IrExpression::Literal(ir::IrLiteral::Number(3.0)),
+    ];
+    let tokens = array_tokens(&items);
+
+    assert_eq!(tokens.to_string(), "[1.0 , 2.0 , 3.0]");
+}
