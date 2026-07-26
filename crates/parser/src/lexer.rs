a99cfb8d28eb8ea8ed62a51eaee07b37a6dbb284
m1rage <miragela.code@gmail.com>
2026-07-26 21:16:53 +0200

    feat(parser): refactor lexer logic

---
+++ crates/parser/src/lexer.rs
@@ -0,0 +1,37 @@
+use swc_common::{SourceFile, comments::SingleThreadedComments};
+use swc_ecma_parser::{Lexer, StringInput, Syntax, TsSyntax};
+
+pub fn lexer<'a>(source: &'a SourceFile, comments: &'a SingleThreadedComments) -> Lexer<'a> {
+    let lexer: Lexer<'_> = Lexer::new(
+        Syntax::Typescript(TsSyntax::default()),
+        swc_ecma_ast::EsVersion::Es5,
+        StringInput::from(source),
+        Some(comments),
+    );
+
+    lexer
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::assert_eq;
+    use swc_common::FileName;
+    use swc_common::sync::Lrc;
+    use swc_common::SourceMap;
+
+    #[test]
+    fn it_converts_sourcefile_to_tokens() {
+        let comments: SingleThreadedComments = SingleThreadedComments::default();
+        let source_map: Lrc<SourceMap> = Default::default();
+        let program = source_map.new_source_file(
+            FileName::Custom("test.js".into()).into(),
+            "function foo() {return 5}",
+        );
+        // Lexer is a just list of tokens (parts of code like 'function', '(', ')', '{'...})
+        let lexer = lexer(program.as_ref(), &comments);
+        let count = lexer.into_iter().count();
+        assert!(count > 0, "Expected more than 0 tokens, but got {}", count);
+        assert_eq!(count, 8, "Expected 8 tokens, but got {}", count);
+    }
+}
--- crates/parser/src/lib.rs
+++ crates/parser/src/lib.rs
@@ -1,43 +1,25 @@
-
 use std::path::Path;
-use std::println;
-use swc_common::comments::SingleThreadedComments;
-use swc_common::sync::Lrc;
-use swc_common::{
-    FileName, SourceMap,
-};
-use swc_ecma_codegen;
-use swc_ecma_parser::{Lexer, Parser, StringInput, Syntax, TsSyntax};
+use swc_common::{SourceMap, comments::SingleThreadedComments, sync::Lrc};
+use swc_ecma_ast::Module;
+use swc_ecma_parser::Parser;
 
-fn parse() -> () {
-    let comments = SingleThreadedComments::default();
+mod lexer;
+
+fn parse() -> Module {
+    let comments: SingleThreadedComments = SingleThreadedComments::default();
+
     // SourceMap manages source files and resolves byte positions to source locations
     // It can inform us about exact position of Error, element, code etc.
     // In the next generation we can add linter using this SourceMap, because we will be able to locate exact 'heavy' function or part of code
     // OR we will be able to show users what is not suppoertd for now
     let source_map: Lrc<SourceMap> = Default::default();
     let program = source_map.load_file(Path::new("./ts.ts")).unwrap();
-    // Lexer is a just list of tokens (parts of code like 'function', '(', ')', '{'...}) 
-    let lexer = Lexer::new(
-        Syntax::Typescript(TsSyntax::default()),
-        swc_ecma_ast::EsVersion::Es5,
-        StringInput::from(program.as_ref()),
-        Some(&comments),
-    );
 
+    // Lexer is a just list of tokens (parts of code like 'function', '(', ')', '{'...})
+    let lexer = lexer::lexer(program.as_ref(), &comments);
     let mut parser = Parser::new_from(lexer);
-    
-        let _module = parser
-        .parse_module()
-        .expect("failed to parser module");
-}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+    let _module = parser.parse_module().expect("failed to parser module");
 
-    #[test]
-    fn test_parse() {
-        parse();
-    }
+    _module
 }
