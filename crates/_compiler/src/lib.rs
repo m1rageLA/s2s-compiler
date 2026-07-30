f0054ebef2d7d21421cd1dad76ee2971c7850403
m1rage <miragela.code@gmail.com>
2026-07-30 22:58:33 +0200

    feat(lowering): add lowering crate and integrate into compiler workflow

--- crates/_compiler/src/lib.rs
+++ crates/_compiler/src/lib.rs
@@ -3,11 +3,9 @@ use parser::parse;
 
 pub fn compileToRust() {
     let source_code = r#"
-
         function add(a, b) {
             return a + b;
         }
-
     "#;
 
     // Parse the source code into an AST module
@@ -21,6 +19,6 @@ pub fn compileToRust() {
     // let hir = hir::lower(normalized_ast);
     // Logger::success("ast-module to hir", "compiler");
 
-    // let rust_code = codegen(normalized_ast);
-    // Logger::success("ir to rust-code", "compiler");
+    let ir = lowering(normalized_ast);
+    Logger::success("ir to rust-code", "compiler");
 }
---
+++ crates/codegen/src/lib.rs
@@ -0,0 +1,6 @@
+use swc_ecma_ast::Module;
+
+pub fn lowering(ast: Module) -> Module {
+    // Implementation for lowering AST to IR
+    ast
+}
